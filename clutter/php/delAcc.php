fe0d051b0fb1f4efab200190fc5cac42de9f00fb
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2018-04-10 20:07:10 +0800

    Clear
    with Milestone specs

--- clutter/php/delAcc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include '../php/dbh.php';
-
-	$id = $_POST['accID'];
-	$sql = "UPDATE users SET uActive=0 WHERE uID=".$id;
-	mysqli_query($conn, $sql);
-	header("Location: ../html/profile.php?mID=".$id);
-?>
