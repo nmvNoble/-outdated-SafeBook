ab533b79e1b35d80387d4fd38bbfda1ebd5e8abd
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2018-04-10 22:41:06 +0800

    the 2nd one
    RAW

--- cart.php
+++ cart.php
@@ -1,291 +1,109 @@
-﻿
 <?php
 
-include("functions/functions.php");
-include("includes/db.php");
-
-cart();
-?>
-
-
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<?php
-
-include("header.php");
 
 ?>
-
-
-    <!-- end of menu tab -->
-    <div class="crumb_navigation"> Navigation: <span class="current">Home</span>
-    
-
-    <?php
-    
-    if(isset($_GET['cat'])){
-    
-    $get_id = $_GET['cat'];
-    
-    $query = "select cat_title from categories where cat_id='$get_id'";
-    $run_query = mysqli_query($con,$query);
-    
-    $row = mysqli_fetch_array($run_query);
-    
-    $cat = $row['cat_title'];
-    
-    echo"<span class=current>>$cat</span>";
-    
-    }
-    
-    ?>
-
-
- </div>
-    <div class="left_content">
-      <div class="title_box">Categories</div>
-      <ul class="left_menu">
-        
- <?php 
-     getcats(); 
-     
-
- ?>
-        
-      </ul>
-     
-<div class="title_box">Manufacturers</div>
-      <ul class="left_menu">
-        
-
-    <?php  getbrands();  ?>
-
-
- 
-</ul>
- <br>
- <br>
-
-     
-      <div class="banner_adds"> <a href="#"><img src="images/bann2.jpg" alt="" border="0" /></a> </div>
-      
-           <br>
- <br>
- <br>
-     
- 
-        <?php
-
-         special();
-
-        ?>
-
-    </div>
-    <!-- end of left content -->
-    <div class="center_content">
-      <div class="center_title_bar">Latest Products</div>
-  
- <form action="" enctype="multipart/form-data" method="post">
- 
- <table align="center" width="500" border="0">
- 
- 
-
-<tr align="center">
-  <th>Remove</th>
-  <th>Product(s)</th>
-  <th>Quantity</th>
-  <th>Total Price</th>
-</tr>
- 
- <?php
- 
-         $total = 0;
-   global $con;
-
-   $ip = getip();
-
-   $price = "select * from cart where ip_add = '$ip'";
-
-   $run_price = mysqli_query($con,$price) ;
-
-   while($pprice = mysqli_fetch_array($run_price)){
-
-      $pro_id = $pprice['p_id'];
-      
-      $prod_price = "select * from products where prd_id = '$pro_id'";
-
-      $run_pro_price = mysqli_query($con,$prod_price);
-
-
-      while($ppprice = mysqli_fetch_array($run_pro_price)){
-
-         $product_price = array($ppprice['prd_price']);
-         $product_title = $ppprice['prd_title'];
-         $product_image = $ppprice['prd_img'];
-         $single_price = $ppprice['prd_price'];
-         
-
-         $price_sum = array_sum($product_price);
-
-         $total +=$price_sum;
-
-         //echo  $product_price;
-         
-
-
-
-
-     
-      
-      
-
-   
-
-      
-
- 
- 
- 
- ?>
- 
- <tr align="center">
-   
-   <td><input type="checkbox" name="remove[]" value="<?php echo $pro_id;  ?>"></td>
-   <td><?php echo $product_title; ?>
-    <br>
-    <img src="admin_area/product_images/<?php echo $product_image; ?>" width="60" height="60"></td>
-   <td>
-   
-   <input type="text" size="4" name="qty">
-   </td>
-   <td>
-   <?php echo "Rs".$single_price;  ?>
-   </td>
-  
-   <td></td>  
-   
- 
- </tr>
- 
- 
- <?php
- 
- }
-   }
- ?>
- 
-  <tr align="right">
-   <td colspan="4"><b>Sub total:</b></td>
-   <td><?php echo"Rs&nbsp;".$total;   ?></td>
-   
-   </tr>
-   
-   
-   <tr align="center">
-   
-   <td colspan="2"><input type="submit" name="update_cart" value="Update cart"></td>
-   
-
-   <td><input type="submit" name="continue" value="Continue Shopping"></td>
-   
-
-  <!-- <td><button><a href="checkout.php" style="text-decoration:none;color:black">Checkout</a></button></td> -->
-   
-   </tr>
-
- </table>
- 
- 
- 
- </form>
- 
- <?php
- 
- $ip = getip();
- 
-
- 
- if(isset($_POST['update_cart'])){
- 
-   foreach($_POST['remove'] as $remove){
-   
-       $delete_product = "delete from cart where p_id='$remove' AND ip_add='$ip'";
-       $run_delete = mysqli_query($con, $delete_product);
-       echo $ip;
-       if($run_delete){
-       
-         echo "<script>window.open('cart.php','_self')</script>"    ;  
-       
-       }
-   
-   }
- 
- }
- 
- ?>
- 
- 
- </div>
-    <!-- end of center content -->
-<div class="right_content">
-      <div class="shopping_cart">
-        <div class="cart_title"><i>Welcome Guest!</i><br>Shopping cart</div>
-        <div class="cart_details"><font color="blue"><?php total_items(); ?></font>&nbsp;items <br />
-          <span class="border_cart"></span> Total:<span class="price"><?php total_price(); ?> </span> </div>
-        <div class="cart_icon"><a href="cart.php" title="header=[Checkout] body=[&nbsp;] fade=[on]"><img src="images/shoppingcart.png" alt="" width="48" height="48" border="0" /></a></div>
-      </div>
-      
-       
-       <br>
-     
-            <?php
- 
-            special();
-
-           ?> 
-
-        <br>
-     
- 
-        <?php
-
-         special();
-
-        ?>
-        
-             <br>
-     
- 
-        <?php
-
-         special();
-
-        ?>
-     <br>
-     
- 
-        <?php
-
-         special();
-
-        ?>
-
-
-      
-        <!-- end of right content -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Khan Store</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
 </div>
-  <!-- end of main content -->
-  <div class="footer">
-    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="170" height="49"/> </div>
-    <div class="center_footer">. All Rights Reserved 2016<br />
-      
-      <img src="images/payment.gif" alt="" /> </div>
-  
-  </div>
-</div>
-<!-- end of main_container -->
-</body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Khan Store</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+			</ul>
+		</div>
+	</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="cart_msg">
+				<!--Cart Message--> 
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Cart Checkout</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-2 col-xs-2"><b>Action</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Image</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Name</b></div>
+							<div class="col-md-2 col-xs-2"><b>Quantity</b></div>
+							<div class="col-md-2 col-xs-2"><b>Product Price</b></div>
+							<div class="col-md-2 col-xs-2"><b>Price in $</b></div>
+						</div>
+						<div id="cart_checkout"></div>
+						<!--<div class="row">
+							<div class="col-md-2">
+								<div class="btn-group">
+									<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+									<a href="" class="btn btn-primary"><span class="glyphicon glyphicon-ok-sign"></span></a>
+								</div>
+							</div>
+							<div class="col-md-2"><img src='product_images/imges.jpg'></div>
+							<div class="col-md-2">Product Name</div>
+							<div class="col-md-2"><input type='text' class='form-control' value='1' ></div>
+							<div class="col-md-2"><input type='text' class='form-control' value='5000' disabled></div>
+							<div class="col-md-2"><input type='text' class='form-control' value='5000' disabled></div>
+						</div> -->
+						<!--<div class="row">
+							<div class="col-md-8"></div>
+							<div class="col-md-4">
+								<b>Total $500000</b>
+							</div> -->
+						</div> 
+					</div>
+					<div class="panel-footer"></div>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+			
+		</div>
+</body>	
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
